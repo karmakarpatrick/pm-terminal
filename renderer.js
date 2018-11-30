c57ec78c5da2ec91fffc0c4ac30f1a8e3030235f
Pratik Karmakar <pratik.karmakar@iic.ac.in>
2018-11-30 09:47:47 +0530

    A simple built-in CPU Usage demo  is incorporated and tested. CSS and JS bundles were pulled and stored locally.

--- renderer.js
+++ renderer.js
@@ -1,3 +1,7 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+alert('hi!! This is my first app on electron. We will be building something good.');
+
+const path = require("path");
+const {db, order} = require(path.resolve('js/cpu'));
